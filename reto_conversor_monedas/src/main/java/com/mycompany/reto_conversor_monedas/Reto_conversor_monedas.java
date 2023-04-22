1392807b3d971622470a479d00f3fce9f8ba67f5
Alejandro <alexalante3@gmail.com>
2023-04-22 15:25:57 -0600

    Creación del proyecto en NetBeans

---
+++ reto_conversor_monedas/src/main/java/com/mycompany/reto_conversor_monedas/Reto_conversor_monedas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto_conversor_monedas;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Reto_conversor_monedas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
